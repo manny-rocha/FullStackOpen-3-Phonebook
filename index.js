4098eb21338bb0370058cd3975e6c74c947a33ab
Manny Rocha <mmrocha905@gmail.com>
2023-02-24 09:03:09 -0500

    added frontend build

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require('cors')
 const app = express()
 
 app.use(cors());
-// app.use(express.static('./build'));
+app.use(express.static('./build'));
 
 morgan.token('req-body', (req, res) => {
     return JSON.stringify(req.body)
