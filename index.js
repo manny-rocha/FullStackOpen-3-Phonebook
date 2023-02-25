0b1d841f3bfbc310ebc43695f11d1e0c3a3ad763
Manny Rocha <mmrocha905@gmail.com>
2023-02-25 10:14:12 -0500

    bringing in frontend

--- index.js
+++ index.js
@@ -1,86 +1,121 @@
-require('dotenv').config()
-const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
 
-const Entry = require('./models/entry')
+const Entry = require("./models/entry");
 
-const app = express()
+const app = express();
 
+app.use(express.static("build"));
 
-app.use(express.static('build'));
-app.use(cors());
-
-app.use(morgan(function (tokens, req, res) {
+app.use(
+  morgan(function (tokens, req, res) {
     return [
       tokens.method(req, res),
       tokens.url(req, res),
       tokens.status(req, res),
-      tokens.res(req, res, 'content-length'), '-',
-      tokens['response-time'](req, res), 'ms',
-      req.method === 'POST' ? JSON.stringify(req.body) : ''
-    ].join(' ')
-}))
+      tokens.res(req, res, "content-length"),
+      "-",
+      tokens["response-time"](req, res),
+      "ms",
+      req.method === "POST" ? JSON.stringify(req.body) : "",
+    ].join(" ");
+  })
+);
 
-app.use(express.json())
+app.use(express.json());
 
-let data = [];
-
-app.get('/info', function (req, res) {
-    let currentDate = new Date();
+app.get("/info", function (req, res) {
+  let currentDate = new Date();
+  
+  Entry.countDocuments({}).then(count => {
     res.send(
         `<!DOCTYPE html>
-        <h2>Phonebook has info for ${ data.length } people</h2>
-        <h3>${ currentDate }</h3>`
-    );
-})
-
-app.get('/api/persons', (req, res) => {
-    Entry.find({}).then()(entries => {
-        res.json(entries);
-    })
+            <h2>Phonebook has info for ${count} people</h2>
+            <h3>${currentDate}</h3>`
+    )
+  })
+    .catch(err => next(err));
 });
 
-app.get('/api/persons/:id', (req, res) => {
-    Entry.findById(req.params.id).then(entry => {
+app.get("/api/persons", (req, res) => {
+  Entry.find({}).then((entries) => {
+    res.json(entries);
+  });
+});
+
+app.get("/api/persons/:id", (req, res, next) => {
+  Entry.findById(req.params.id)
+    .then((entry) => {
+      if (entry) {
         res.json(entry);
-    });
+      } else {
+          res.status(404).end();
+      }
+    })
+    .catch(err => next(err));
 });
 
-app.post('/api/persons', (req, res) => {
+app.post("/api/persons", (req, res, next) => {
+  const body = req.body;
+
+  if (body.name === undefined || body.number === undefined) {
+    return res.status(400).json({
+      error: "Name or number not provided",
+    });
+  }
+
+  const entry = new Entry({
+    name: body.name,
+    number: body.number,
+  });
+
+  entry.save().then((savedEntry) => {
+    res.json(savedEntry);
+  });
+});
+
+app.put("/api/persons/:id", (req, res, next) => {
     const body = req.body;
 
-    if (body.name === undefined || body.number === undefined) {
-        return res.status(400).json({
-            error: 'Name or number not provided'
-        })
-    }
-
-    // const duplicates = data.some(person => person.name  === body.name || person.number === body.number)
-    // if (duplicates) {
-    //     return res.status(400).json({
-    //         error: 'Name or number already exists'
-    //     })
-    // }
-
-    const entry = new Entry({
+    const updatedEntry ({
         name: body.name,
-        number: body.number,
+        number: body.number
     })
-    
-    entry.save().then(savedEntry => {
-        res.json(savedEntry)
-    }) 
+
+    Entry.findByIdAndUpdate(req.params.id, updatedEntry, { new: true }).then(result => {
+        res.json(result);
+    })
+    .catch(err => next(err));
 });
 
-app.delete('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id);
-    data = data.filter(person => person.id !== id);
-
-    res.status(204).end();
+app.delete("/api/persons/:id", (req, res) => {
+    Entry.findByIdAndRemove(req.params.id)
+        .then(result => {
+            res.status(204).end();
+        })
+        .catch(err => next(err));
 });
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-  })
+  console.log(`Server running on port ${PORT}`);
+});
+
+const unknownEndpoint = (req, res) => {
+    res.status(404).send({ error: 'unknown endpoint' })
+};
+
+app.use(unknownEndpoint);
+
+const errorHandler = (err, req, res, next) => {
+    console.error(err.message);
+
+    if (err.name === 'CastError') {
+        return res.status(400).send({ error: 'malformatted id' })
+    }
+    
+    next(err);
+};
+
+app.use(errorHandler);
